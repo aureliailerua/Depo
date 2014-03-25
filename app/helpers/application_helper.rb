a3cb5809a57fd09fa0ed2b0d690c8104822bc42b
aureliailerua <aureliailerua@gmail.com>
2014-03-25 15:06:20 +0100

    hiding empty carts_ok

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# By using the &block notation, we get Ruby to pass the 
+# block that was given to hidden_div_if() down to content_tag().
+
+
 module ApplicationHelper
+	def hidden_div_if(condition, attributes = {}, &block)
+		if condition
+			attributes["style"] = "display: none"
+		end
+		content_tag("div", attributes, &block)
+	end
 end
